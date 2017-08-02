8f1a81ba0ebc4baff181b8cef1c5058cfc8feedb
Ivan Duran <idurandiaz@gmail.com>
2017-08-02 13:14:33 +0500

    Add animation to navbar when scrolling

--- FrontEndDevelop/Basic_frontEnd_projects/portfolio_page/portfolio_page.js
+++ FrontEndDevelop/Basic_frontEnd_projects/portfolio_page/portfolio_page.js
@@ -1,3 +1,38 @@
+// hide navbar on scrolling down
+// let prevScrollPos = 0;
+
+// document.addEventListener('scroll', () => {
+//   let nextScrollPos = window.scrollY;
+//   let scrollAmount = nextScrollPos - prevScrollPos;
+//   if (scrollAmount !== 0) { prevScrollPos = nextScrollPos };
+//   console.log(scrollAmount);
+//   // if (scrollAmount > 30) { $( 'nav' ).hide(1000) };
+//   if (scrollAmount > 100) { $( 'nav' ).addClass('animated slideOutUp') };
+//   // if (scrollAmount < -30) { $( 'nav' ).show(1000) };
+//   if (scrollAmount < -10) { 
+//     $( 'nav' ).removeClass('animated slideOutUp')
+//     $( 'nav' ).addClass('animated slideInDown')
+//   };
+// })
+
+$(document).ready(function() {
+    // Put your offset checking in a function
+    function checkOffset() {
+        if ($(".navbar").offset().top > 100) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        }     
+        else {
+            $(".fixed-top").removeClass("top-nav-collapse");
+        }
+    }
+    // Run it when the page loads
+    checkOffset();
+    // Run function when scrolling
+    $(window).scroll(function() {
+        checkOffset();
+    });
+});
+
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
